7e01ff9b89a7f5d26a565880d86c19b91337d642
Esra Al <esra.wameed@yahoo.com>
2022-10-23 15:11:08 -0400

    added a post route to create new blog

--- controllers/blog-routes.js
+++ controllers/blog-routes.js
@@ -28,4 +28,15 @@ router.get('/blog/:id', async (req, res) => {
     };     
 });
 
+//route to post new blog
+router.post('/', async (req, res) => {
+    // create a new blog
+    try {
+      const newBlog = await Blog.create(req.body);
+      res.status(200).json(newBlog);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+
 module.exports = router;
